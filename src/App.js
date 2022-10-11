0a0978f78e91ceab95cf76236c7d8fa8aa2e8fb7
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-10-11 19:40:27 -0400

    Number input functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import react, { useState } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [number, setNumber] = useState("");
+  const handleClick = (e) => {
+    getQuotes(e);
+  };
+  const getQuotes = (e) => {
+    e.preventDefault();
+    console.log(number);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quote Generator</h1>
+      <h2>Number of quotes</h2>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        ></input>
+      </form>
+      <button
+        onClick={(e) => {
+          handleClick(e);
+        }}
+      >
+        Get Quotes
+      </button>
+      <div>{quotes}</div>
     </div>
   );
 }
