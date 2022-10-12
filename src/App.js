d8f25635bac546a18d0c7edca1b16f3955c96eab
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-10-11 20:24:13 -0400

    Functionality complete, needs CSS work

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import react, { useState } from "react";
 
 function App() {
+  const url = "https://type.fit/api/quotes";
   const [quotes, setQuotes] = useState([]);
   const [number, setNumber] = useState("");
   const handleClick = (e) => {
@@ -10,8 +11,46 @@ function App() {
   };
   const getQuotes = (e) => {
     e.preventDefault();
+    if (number === "0") {
+      alert("Please enter a number");
+    } else {
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          data = shuffle(data);
+          let output = [];
+          for (let i = 0; i < data.length; i++) {
+            if (i == number) {
+              break;
+            }
+            output.push([data[i].text, data[i].author]);
+          }
+          setQuotes(output);
+        });
+      console.log(quotes);
+    }
+
     console.log(number);
   };
+  function shuffle(quotes) {
+    let CI = quotes.length,
+      tempValue,
+      randomIndex;
+
+    // While elements exist in the array
+    while (CI > 0) {
+      randomIndex = Math.floor(Math.random() * CI);
+      // DECREASE CI BY 1
+      CI--;
+      // SWAP THE LAST ELEMENT WITH CI
+      tempValue = quotes[CI];
+      quotes[CI] = quotes[randomIndex];
+      quotes[randomIndex] = tempValue;
+    }
+    return quotes;
+  }
 
   return (
     <div className="App">
@@ -31,7 +70,16 @@ function App() {
       >
         Get Quotes
       </button>
-      <div>{quotes}</div>
+      <div>
+        {quotes.map((quote, index) => {
+          return (
+            <div key={index} className="singleQuote">
+              <div className="quote"> {quote[0]}</div>
+              <div className="author">~ {quote[1]}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
